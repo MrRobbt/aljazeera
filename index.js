4875517327cc52fce39ff58a2dc6f9782b56f425
TJimmY123 <56742682+TJimmY123@users.noreply.github.com>
2021-05-03 00:39:01 -0700

    aljazeera
    aljazeera

---
+++ index.js
@@ -0,0 +1,26 @@
+const { TOKEN, CHANNEL, SERVER, STATUS, LIVE } = require("./config.json");
+const discord = require("discord.js");
+const client = new discord.Client();
+const ytdl = require('ytdl-core');
+const keepAlive =require("./server");
+
+client.on('ready', async () => {
+  client.user.setActivity(STATUS + " 📻")
+  let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+
+  if(!channel) return;
+  const connection = await channel.join();
+  connection.play(ytdl(LIVE))
+})
+
+setInterval(async function() {
+  if(!client.voice.connections.get(SERVER)) {
+    let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+    if(!channel) return;
+
+    const connection = await channel.join()
+    connection.play(ytdl(LIVE))
+  }
+}, 20000)
+keepAlive();
+client.login(TOKEN)
